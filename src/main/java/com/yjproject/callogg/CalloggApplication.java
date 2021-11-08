a41c8f07af01fe9d3d0d0751d62764c8f248ff7f
yj <qlrogo91lp@gmail.com>
2021-11-08 23:26:09 +0900

    Initial commit

---
+++ src/main/java/com/yjproject/callogg/CalloggApplication.java
@@ -0,0 +1,13 @@
+package com.yjproject.callogg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalloggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalloggApplication.class, args);
+    }
+
+}
